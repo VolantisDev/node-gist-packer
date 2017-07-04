2635b97113980d41baa566075041c82178914704
Ben McClure <ben.mcclure@gmail.com>
2017-07-04 11:02:15 -0500

    Resolve a couple of bugs

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+const bundle = require('./bundle')
+
 module.exports = cfg => {
     const config = getConfig(cfg)
-    const gist = require('./lib/gist')
+    const gist = require('./lib/gist')(config)
     const meta = require('./lib/meta')(config)
     const packer = require('./lib/packer')(config)
-    
 
     return {
         config,
         gist,
         meta,
-        packer
+        packer,
+        bundle
     }
 }
 
